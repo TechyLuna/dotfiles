88707e7ddd5d7a8ff0d9f656eb34c5df59a6f302
GearKite <onyx-commute-quit@duck.com>
2024-10-23 11:41:06 +0300

    Switch kb_layout to lv

--- home/desktop/hyprland/settings/input.nix
+++ home/desktop/hyprland/settings/input.nix
@@ -2,7 +2,7 @@
 {
   wayland.windowManager.hyprland.settings = {
     input = {
-      kb_layout = "us";
+      kb_layout = "lv";
       kb_variant = "";
       kb_model = "";
       kb_options = "";
