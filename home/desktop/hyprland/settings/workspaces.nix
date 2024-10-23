59e6fb5bac7a63cc74358c81b5ef894228308060
GearKite <onyx-commute-quit@duck.com>
2024-10-23 22:45:14 +0300

    Add default workspaces

--- home/desktop/hyprland/settings/workspaces.nix
+++ home/desktop/hyprland/settings/workspaces.nix
@@ -2,11 +2,11 @@
 {
   wayland.windowManager.hyprland.settings = {
     workspace = [
-      "1, monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 21502B003480"
+      "1, monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 21502B003480, default:true"
       "2, monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 21502B003480"
       "3, monitor:desc:GIGA-BYTE TECHNOLOGY CO. LTD. G27Q 21502B003480"
 
-      "4, monitor:desc:LG Electronics L194WT 0x000627AF"
+      "4, monitor:desc:LG Electronics L194WT 0x000627AF, default:true"
       "5, monitor:desc:LG Electronics L194WT 0x000627AF"
       "6, monitor:desc:LG Electronics L194WT 0x000627AF"
     ];
