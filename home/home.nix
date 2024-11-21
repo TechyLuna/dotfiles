d8bfe1d27a558f6aa7e78f0fa11fbb6ee787bdbb
GearKite <onyx-commute-quit@duck.com>
2024-11-21 14:01:00 +0200

    move zsh config to separate file

--- home/home.nix
+++ home/home.nix
@@ -3,6 +3,7 @@
   imports = [
     ./desktop
     ./browser
+    ./shell
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
@@ -78,25 +79,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    shellAliases = {
-      ll = "ls -l";
-      update = "sudo nixos-rebuild switch";
-    };
-    history = {
-      size = 10000;
-      path = "${config.xdg.dataHome}/zsh/history";
-    };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "git" "thefuck" "direnv" ];
-      theme = "agnoster";
-    };
-  };
 }
