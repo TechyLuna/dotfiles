6631d2f85a79875db5daaa9a767e94eb625afed6
Luna <techyluna@proton.me>
2024-11-26 20:37:34 +0200

    disabled xwayland scaling

--- home/desktop/hyprland/settings/windowrules.nix
+++ home/desktop/hyprland/settings/windowrules.nix
@@ -3,5 +3,8 @@
   wayland.windowManager.hyprland.settings = {
     windowrule = "workspace 4 silent, WebCord";
     windowrulev2 = "workspace 4 silent, title:Steam";
+    xwayland = {
+    force_zero_scaling = true;
+    };
    };
 }
