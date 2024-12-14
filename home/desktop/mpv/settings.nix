15bdb0dba66d824bb7a52675018e31d36cb4d3a0
Luna <techyluna@proton.me>
2024-12-14 22:49:40 +0200

    removed specific audio codecs

--- home/desktop/mpv/settings.nix
+++ home/desktop/mpv/settings.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   programs.mpv.config = {
-    audio-spdif= ["ac3" "dts" "eac3"];
-    audio-channels= "3.0";
+    # audio-spdif= ["ac3" "dts" "eac3"];
+    audio-channels= "5.0";
     };
 }
