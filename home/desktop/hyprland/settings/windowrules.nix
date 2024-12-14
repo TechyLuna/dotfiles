bb611ab1067ddb39ff12575d25c485c4cc734d03
TechyLuna <techyluna@proton.me>
2024-12-14 19:26:44 +0200

    float pavu and calc

--- home/desktop/hyprland/settings/windowrules.nix
+++ home/desktop/hyprland/settings/windowrules.nix
@@ -2,7 +2,17 @@
 {
   wayland.windowManager.hyprland.settings = {
     windowrule = "workspace 4 silent, WebCord";
-    windowrulev2 = "workspace 4 silent, title:Steam";
+    windowrulev2 = [
+    "workspace 4 silent, title:Steam"
+    
+    "float, title:Qalculate!"
+    "size > >, title:Qalculate!"
+    
+    "float, class:pavucontrol"
+    "size > >, class:pavucontrol"
+
+    ];
+
     xwayland = {
     force_zero_scaling = true;
     };
