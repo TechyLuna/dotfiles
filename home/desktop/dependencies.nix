b4088921c330243e91f9bf69509c31c4b2e0fbb5
Luna <techyluna@proton.me>
2024-12-15 04:39:54 +0200

    replaced lunarvim with nvim and astrovim setup in the init.lua

--- home/desktop/dependencies.nix
+++ home/desktop/dependencies.nix
@@ -6,9 +6,20 @@
     hyprshot
     satty
     webcord
+    fastfetch
+    alacritty
+
+
+    neovim
+    gcc
+    cargo
+    nodejs_23
 
     cliphist
     wl-clipboard
     jq # logout script
   ];
+  btop.override = {
+    rocmSupport = true;
+  };
 }
