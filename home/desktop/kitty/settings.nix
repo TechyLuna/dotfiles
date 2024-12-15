95c7a85f34a1d5bd0a1592bd5fdd48849055bdb9
Luna <techyluna@proton.me>
2024-12-15 13:44:35 +0200

    changed opacity of kitty

--- home/desktop/kitty/settings.nix
+++ home/desktop/kitty/settings.nix
@@ -3,7 +3,7 @@
   programs.kitty = {
     settings = {
       background_blur = "1";
-      background_opacity = "0.3";
+      background_opacity = "0.8";
       dynamic_background_opacity = true;
     };
     shellIntegration = {
